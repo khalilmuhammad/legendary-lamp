b17a12e390348b7d0fd9c54e1caae4fd0d8ff64b
khalilmuhammad <40534693+khalilmuhammad@users.noreply.github.com>
2019-02-23 01:04:36 -0500

    Add files via upload

---
+++ f drive/windows/app/config.js
@@ -0,0 +1,79 @@
+/* globals exports */
+'use strict';
+
+exports.ids = {
+  chrome: [
+    'lmeddoobegbaiopohmpmmobpnpjifpii', // Open in Firefox (Chrome)
+    'hhalgjmpmjelidhhjldondajffjbcmcg', // Open in Firefox (Opera)
+    'poibpkhpegdblnblbkcppknekhkhmmlp', // Open in Edge (Chrome)
+    'mjoebkkejejidnkfdekpbooceogbapnf', // Open in Edge (Chrome) by @rynu.smith
+    'jjlkfjglmelpbkgfeebcdipalggpapag', // Open in Edge (Opera)
+    'amojccmdnkdlcjcplmkijeenigbhfbpd', // Open in Opera (Chrome)
+    'eoenobhieedpimobhembbgdcdhdcdcig', // Open in Safari (Chrome)
+    'bdlaolidgmapdilglbocpmbfipdpfjef', // Open in Safari (Opera)
+    'noedpljikmfalclpadmnjbabbignpfge', // Open in IE (Opera)
+    'agaecbnjediafcdopcfidcdiponjlmnk', // Open in IE (Chrome)
+    'looohpideggedchhpphemdmppnmdkgfd', // Open in IE (Chrome) by @rynu.smith
+    'ocnfecjfebnllnapjjoncgjnnkfmobjc', // Media Converter (Chrome) [By @InBasic]
+    'fabccabfpmdadbhljpcmcbmepepfllnb', // Media Converter (Opera) [By @InBasic]
+    'jgpghknlbaljigdhcjimjnkkjniiipmm', // Open in GIMP (Chrome)
+    'cmjahocdpafkodabbojjaebogoigcipj', // Open in GIMP (Opera)
+    'bifmfjgpgndemajpeeoiopbeilbaifdo', // External Application Button (Chrome)
+    'enemdfoackoekaedijjmjlckkleokhih', // External Application Button (Opera)
+    'ihpiinojhnfhpdmmacgmpoonphhimkaj', // Open in VLC (Chrome)
+    'jfpmkcifglhenaihlnefaccjbmcmbigf', // Open in VLC (Opera)
+    'gmdcejkkcimnbfmicdpohgicekomipde', // Open in Chrome (Opera)
+    'gkmonffckeeffppchajngpdakfppalfo', // Auto Shutdown (Chrome)
+    'iajkcajifgglcaojccgiohphgjmoleia', // Auto Shutdown (Opera)
+    'klglmiipkjlhpeeopnnlkhdomjhbkpjh', // Open in Vivaldi (Chrome)
+    'ldoihpohemnbokndocafmiblepbjeacb', // Open in Vivaldi (Opera)
+    'dijcjcnibopfdgbmpkgnjfdihfdeghcc', // Open in Tor Browser (Chrome)
+    'emlgnifoahkaeocfmafkpnfbijoioefh', // Open in Tor Browser (Opera)
+    'kjoabfljeghcinlpjhdbdfbcflapkccm', // Tor Control (Chrome)
+    'knfbgpkbkfebddfbklfpgmdjgolnkkfl', // Tor Control (Opera)
+    'khgbdhkpcapllhgfekjegcinegfhjbmi', // YVAD by (Opera) [By @InBasic]
+    'cehiomcamjpnfmemkmpjadaclohoibgo', // Open in PDF viewer (Chrome)
+    'honagjpkinogkppkapphkpdffnegagge', // Open in PDF viewer (Opera)
+    'nfpgfobeckckemhmggkdfjkjaiikadnd', // Open in Yandex (Chrome)
+    'dlobcokaeaiaihfebfhpdoongldphgkf', // Open in Yandex (Opera)
+    'bffjckjhidlcnenenacdahhpbacpgapo', // Country Flags (Chrome)
+    'ljealnphmlekclcdoeficainfegfjgkc', // Country Flags (Opera)
+    'djlddpgigallklconlgapabopblddaki', // Edit with Photoshop (Chrome) [By @ray-lothian]
+    'babedbfkpnfmnopiagfocoelpfmpldkc', // Edit with Photoshop (Opera) [By @ray-lothian]
+    'kakiifnbkffafinaifemmingfmfjdklh', // Edit with Paint.Net (Chrome) [By @ray-lothian]
+    'cbdomccfnegbmmpfakfpnonihdnfmonn', // Edit with Paint.Net (Opera) [By @ray-lothian]
+    'oahnpfaakepnaabalmemoljdonbdjeei', // Easy Video Downloader (Opera) [By @ray-lothian]
+    'eaicplkoeceoelookkiaeekhodehdhde', // Easy Video Downloader (Chrome) [By @ray-lothian]
+    'oofmnabdpcibefadlibdpnnbglcehfpj', // Email Client for Notmuch [By @InBasic]
+    'ghnflaojldkldmjdiocenfmeonemoogb', // Email Client for Notmuch [By @InBasic]
+    'lhplfipknbnglagbgbfogdaihdcekfga', // Open in Foxit Reader [By @lunu-bounir]
+    'dkkkgkdappindobbhmcjnjidkggbdiil', // Open in PDF-XChange Viewer [By @lunu-bounir]
+    'bhfenhhfpcpkknkahnlogooiodcofkjl', // Open in Chromium (Chrome)
+    'dilpigogejfnnahbbmnpjmccammdaiom', // Open in Chromium (Opera)
+  ],
+  firefox: [
+    '{5610edea-88c1-4370-b93d-86aa131971d1}', // Open in IE
+    '{94782f74-1a58-4332-a803-00006221a9d0}', // Open in Edge
+    '{3128fe8f-f039-42ea-8b12-126f78387074}', // Open in Safari
+    '{8db82a75-48fd-452a-81cf-bd40b2e60dac}', // Open in Opera
+    '{42dce5a2-467b-4d7b-ad11-50f4ab3d03cf}', // Open in Yandex
+    '{0ff128a1-c286-4e73-bffa-9ae879b244d5}', // Media Converter
+    '{65b77238-bb05-470a-a445-ec0efe1d66c4}', // External Application Button
+    '{086f665e-6a55-4107-9147-f9a14e72b137}', // Open in Chrome
+    '{6b954d17-d17c-4a19-8fe6-ee8052a562d6}', // Open in VLC
+    '{9a71ec90-d0b6-44af-833f-efe418ff8454}', // Auto Shutdown
+    '{0bcb72f8-7da8-4071-905c-2de13f5aad3a}', // Open in Vivaldi
+    '{9d3b260b-886d-4263-b9d6-81d756ee4929}', // Open in Tor Browser
+    '{f73df109-8fb4-453e-8373-f59e61ca4da3}', // YVAD [By @InBasic]
+    '{d22a1484-dcef-44e9-ab52-80f0f4a331a3}', // Tor Control
+    '{0d3afca0-aedf-491f-b0f9-9ffc22113ea8}', // Open in PDF viewer
+    '{802a552e-13d1-4683-a40a-1e5325fba4bb}', // Country Flag
+    '{e127aaca-d162-4e4c-8de6-a5ee4de1a2ec}', // Edit with Photoshop [By @ray-lothian]
+    '{3e8ae4b2-678d-4a63-8104-4d4d8d3b4f46}', // Edit with Paint.Net [By @ray-lothian]
+    '{cd04e15e-6b23-4648-860d-0057602a5c2a}', // Easy Video Downloader [By @ray-lothian]
+    '{8e409c88-e088-4ce8-8506-5a91e6c502a8}', // Email Client for Notmuch [By @InBasic]
+    '{655859e0-3c86-43a1-9794-88721dacc481}', // Open in Foxit Reader [By @lunu-bounir]
+    '{00186e07-f704-41ce-90aa-b09d4f49a7db}', // Open in PDF-XChange Viewer [By @lunu-bounir]
+    '{4d3bd246-4326-4ec1-bb49-a27cfd57ca08}', // Open in Chromium
+  ]
+};
---
+++ f drive/windows/app/host.js
@@ -0,0 +1,488 @@
+/* globals global, require, process, Buffer */
+'use strict';
+
+function lazyRequire(lib, name) {
+  if (!name) {
+    name = lib;
+  }
+  global.__defineGetter__(name, function() {
+    return require(lib);
+  });
+  return global[name];
+}
+
+var spawn = require('child_process').spawn;
+var fs = lazyRequire('fs');
+var net = lazyRequire('net');
+var os = lazyRequire('os');
+var path = lazyRequire('path');
+var http = lazyRequire('./follow-redirects').http;
+var https = lazyRequire('./follow-redirects').https;
+
+var server;
+var files = [];
+var sprocess = [];
+
+var config = {
+  version: '0.7.0'
+};
+// closing node when parent process is killed
+process.stdin.resume();
+process.stdin.on('end', () => {
+  files.forEach(file => {
+    try {
+      fs.unlink(file);
+    }
+    catch (e) {}
+  });
+  sprocess.forEach(ps => ps.kill());
+  try {
+    server.close();
+    server.unref();
+  }
+  catch (e) {}
+  process.exit();
+});
+
+/////////////////process.on('uncaughtException', e => console.error(e));
+
+function observe(msg, push, done) {
+  if (msg.cmd === 'version') {
+    push({
+      version: config.version,
+    });
+    done();
+  }
+  if (msg.cmd === 'spec') {
+    push({
+      version: config.version,
+      env: process.env,
+      separator: path.sep,
+      tmpdir: os.tmpdir()
+    });
+    done();
+  }
+  else if (msg.cmd === 'echo') {
+    push(msg);
+    done();
+  }
+  else if (msg.cmd === 'spawn') {
+    if (msg.env) {
+      msg.env.forEach(n => process.env.PATH += path.delimiter + n);
+    }
+    const p = Array.isArray(msg.command) ? path.join(...msg.command) : msg.command;
+    const sp = spawn(p, msg.arguments || [], Object.assign({env: process.env}, msg.properties));
+
+    if (msg.kill) {
+      sprocess.push(sp);
+    }
+
+    sp.stdout.on('data', stdout => push({stdout}));
+    sp.stderr.on('data', stderr => push({stderr}));
+    sp.on('close', code => {
+      push({
+        cmd: msg.cmd,
+        code
+      });
+      done();
+    });
+    sp.on('error', e => {
+      push({
+        code: 1007,
+        error: e.message
+      });
+      done();
+    });
+  }
+  else if (msg.cmd === 'clean-tmp') {
+    files.forEach(file => {
+      try {
+        fs.unlink(file);
+      }
+      catch (e) {}
+    });
+    files = [];
+    push({
+      code: 0
+    });
+    done();
+  }
+  else if (msg.cmd === 'ifup') {
+    server = http.createServer(function(req, res) {
+      if (req.headers['api-key'] !== msg.key) {
+        res.statusCode = 400;
+        return res.end('HTTP/1.1 400 Bad API Key. Restarting application may fix this.');
+      }
+      if (req.method === 'PUT') {
+        let filename = req.headers['file-path'];
+        if (filename.startsWith('enc:')) {
+          filename = decodeURIComponent(filename.substr(4));
+        }
+        files.push(filename);
+        const file = fs.createWriteStream(filename);
+        req.pipe(file);
+        file.on('finish', () => {
+          file.close(() => {
+            res.statusCode = 200;
+            res.end('File is stored locally');
+          });
+        });
+        file.on('error', e => {
+          res.statusCode = 400;
+          res.end('HTTP/1.1 400 Bad Request');
+        });
+      }
+    });
+    server.on('error', e => {
+      push({
+        error: e.message,
+        code: 1006
+      });
+      done();
+    });
+    server.listen(msg.port, () => {
+      push({
+        code: 0,
+        msg: 'Server listening on: http://localhost:' + msg.port
+      });
+      done();
+    });
+  }
+  else if (msg.cmd === 'exec') {
+    if (msg.env) {
+      msg.env.forEach(n => process.env.PATH += path.delimiter + n);
+    }
+    const p = Array.isArray(msg.command) ? path.join(...msg.command) : msg.command;
+    const sp = spawn(p, msg.arguments || [], Object.assign({
+      env: process.env,
+      detached: true
+    }, msg.properties));
+    if (msg.kill) {
+      sprocess.push(sp);
+    }
+    let stderr = '';
+    let stdout = '';
+    sp.stdout.on('data', data => stdout += data);
+    sp.stderr.on('data', data => stderr += data);
+    sp.on('close', code => {
+      push({
+        code,
+        stderr,
+        stdout
+      });
+      done();
+    });
+  }
+  else if (msg.cmd === 'dir') {
+    const files = [];
+    const folders = [];
+    const walk = (dir, depth = 0) => {
+      const list = fs.readdirSync(dir);
+      list.forEach(file => {
+        file = path.join(dir, file);
+        const stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) {
+          folders.push(file);
+          if (depth < msg.depth) {
+            return walk(file, depth + 1);
+          }
+        }
+        else {
+          files.push(file);
+        }
+      });
+      return;
+    };
+    if (msg.recursive) {
+      try {
+        walk(msg.path);
+        const rtn = {
+          code: 0,
+          folders,
+          separator: path.sep
+        };
+        if (msg.files) {
+          rtn.files = files;
+        }
+        push(rtn);
+      }
+      catch (e) {
+        push({
+          error: e.message,
+          code: 1003
+        });
+      }
+      done();
+    }
+    else {
+      fs.readdir(msg.path, (error, files) => {
+        if (error) {
+          push({
+            error: `Cannot open directory; number=${error.errno}, code=${error.code}`,
+            code: 1002
+          });
+        }
+        else {
+          push({
+            files,
+            folders: files.filter(file => fs.statSync(path.join(msg.path, file)).isDirectory()),
+            separator: path.sep
+          });
+        }
+        done();
+      });
+    }
+  }
+  else if (msg.cmd === 'env') {
+    push({
+      env: process.env
+    });
+    done();
+  }
+  /* cmd === download is deprecated. Use browser's built-in download manager */
+  else if (msg.cmd === 'download') {
+    const file = fs.createWriteStream(msg.filepath);
+    const request = https.get({
+      hostname: msg.hostname,
+      port: msg.port,
+      path: msg.path
+    }, response => {
+      const size = parseInt(response.headers['content-length']);
+      response.pipe(file);
+      file.on('finish', () => {
+        if (msg.chmod) {
+          fs.chmodSync(msg.filepath, msg.chmod);
+        }
+        file.close(() => {
+          const s = fs.statSync(msg.filepath).size;
+          push({
+            size,
+            path: msg.filepath,
+            code: s === size ? 0 : 1004,
+            error: s !== size ? `file-size (${s}) does not match the header content-length (${size}).
+              Link: ${msg.hostname}/${msg.path}` : null
+          });
+          done();
+        });
+      });
+    });
+    request.on('error', err => {
+      fs.unlink(msg.filepath);
+      push({
+        error: err.message,
+        code: 1001
+      });
+      done();
+    });
+    request.on('socket', function(socket) {
+      socket.setTimeout(msg.timeout || 60000);
+      socket.on('timeout', () => request.abort());
+    });
+  }
+  else if (msg.cmd === 'save-data') {
+    const matches = msg.data.match(/^data:.+\/(.+);base64,(.*)$/);
+    if (matches && matches.length) {
+      const ext = matches[1];
+      const data = matches[2];
+      const buffer = new Buffer(data, 'base64');
+
+      fs.mkdtemp(os.tmpdir(), (err, folder) => {
+        if (err) {
+          push({
+            error: err.message,
+            code: 1007
+          });
+          done();
+        }
+        const file = path.join(folder, 'image.' + ext);
+        fs.writeFile(file, buffer, err => {
+          if (err) {
+            push({
+              error: err.message,
+              code: 1006
+            });
+            done();
+          }
+          else {
+            push({
+              code: 0,
+              file
+            });
+            done();
+          }
+        });
+      });
+    }
+    else {
+      push({
+        error: 'cannot parse data-uri',
+        code: 1005
+      });
+      done();
+    }
+  }
+  else if (msg.cmd === 'net') {
+    let stdout = '';
+    const connection = net.connect({
+      port: msg.port,
+      host: msg.host,
+      persistent: false
+    });
+    connection.on('end', () => {
+      push(stdout);
+      done();
+    });
+    connection.on('data', data => {
+      data = data.toString();
+      stdout += data;
+    });
+    msg.commands.forEach(cmd => connection.write(cmd));
+  }
+  else if (msg.cmd === 'copy') {
+    let cbCalled = false;
+    const end = error => {
+      if (cbCalled === false) {
+        push(error ? {
+          error,
+          code: 1010
+        } : {
+          code: 0,
+          target: msg.target
+        });
+        done();
+        cbCalled = true;
+      }
+    };
+    const rd = fs.createReadStream(msg.source);
+    rd.on('error', e => end(e));
+    const wr = fs.createWriteStream(msg.target);
+    wr.on('error', e => end(e));
+    wr.on('finish', () => {
+      if (msg.chmod) {
+        fs.chmodSync(msg.target, msg.chmod);
+      }
+      if (msg.delete) {
+        fs.unlink(msg.source, error => {
+          if (error) {
+            return end(error);
+          }
+          end();
+        });
+      }
+      else {
+        end();
+      }
+    });
+    rd.pipe(wr);
+  }
+  else if (msg.cmd === 'remove') {
+    const unlink = file => new Promise((resolve, reject) => {
+      fs.unlink(file, error => {
+        if (error) {
+          return reject(error);
+        }
+        resolve();
+      });
+    });
+    Promise.all(msg.files.map(file => unlink(file))).then(
+      () => {
+        push({
+          code: 0
+        });
+        done();
+      },
+      error => {
+        push({
+          error,
+          code: 1011
+        });
+        done();
+      }
+    );
+  }
+  else if (msg.cmd === 'move') {
+    const one = () => {
+      const obj = msg.pairs.shift();
+      if (obj) {
+        let destination = obj.destination;
+        if (msg.guess) {
+          destination = path.join(destination, path.parse(obj.file).base);
+        }
+        const is = fs.createReadStream(obj.file);
+        const os = fs.createWriteStream(destination);
+        is.on('end', () => {
+          fs.unlinkSync(obj.file);
+          one();
+        });
+        is.on('error', error => {
+          push({
+            error,
+            code: 1020
+          });
+          done();
+        });
+        is.pipe(os);
+      }
+      else {
+        push({
+          code: 0
+        });
+        done();
+      }
+    };
+    one();
+  }
+  // this is from openstyles/native-client
+  else if ('script' in msg) {
+    let close;
+    const exception = e => {
+      push({
+        code: -1,
+        type: 'exception',
+        error: e.stack
+      });
+      close();
+    };
+    close = () => {
+      process.removeListener('uncaughtException', exception);
+      done();
+      close = () => {};
+    };
+    process.addListener('uncaughtException', exception);
+
+    const vm = require('vm');
+    const sandbox = {
+      version: config.version,
+      env: process.env,
+      push,
+      close,
+      setTimeout,
+      args: msg.args,
+      // only allow internal modules that extension already requested permission for
+      require: name => (msg.permissions || []).indexOf(name) === -1 ? null : require(name)
+    };
+    const script = new vm.Script(msg.script);
+    const context = new vm.createContext(sandbox);
+    script.runInContext(context);
+  }
+  else {
+    push({
+      error: 'cmd is unknown',
+      cmd: msg.cmd,
+      code: 1000
+    });
+    done();
+  }
+}
+/* message passing */
+var nativeMessage = require('./messaging');
+
+var input = new nativeMessage.Input();
+var transform = new nativeMessage.Transform(observe);
+var output = new nativeMessage.Output();
+
+process.stdin
+    .pipe(input)
+    .pipe(transform)
+    .pipe(output)
+    .pipe(process.stdout);
